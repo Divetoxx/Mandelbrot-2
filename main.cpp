16f34df801f896a5568c247258ddf173586b0ef3
Mandelbrot <aokoroko@gmail.com>
2026-08-09 17:10:18 +0300

    Add

--- main.cpp
+++ main.cpp
@@ -138,7 +138,7 @@ void thread_mandelbrot_calc() {
         double ss_step_d = p.step_d;
         double limit_epsilon_squared;
         mpfr_exp_t current_binary_exp = mpfr_get_exp(st);
-        if (current_binary_exp > -244) {
+        if (current_binary_exp > -70) {
             limit_epsilon_squared = 1e-20;
         } else {
             limit_epsilon_squared = 1e-60;
